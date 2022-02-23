11b5c5d3f7b3685954e8b602458330355ef1959e
lucas <Lucas-ohmygoal>
2022-02-23 14:56:49 +0000

    to move the robot

---
+++ build/nao_control/CMakeFiles/nao_control.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nao_control.dir/src/nao_control.cpp.o"
+  "/home/lucas/nao_soccer_ws/devel/lib/nao_control/nao_control.pdb"
+  "/home/lucas/nao_soccer_ws/devel/lib/nao_control/nao_control"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nao_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/nao_description/CMakeFiles/base_footprint.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/base_footprint.dir/src/base_footprint.cpp.o"
+  "/home/lucas/nao_soccer_ws/devel/lib/nao_description/base_footprint.pdb"
+  "/home/lucas/nao_soccer_ws/devel/lib/nao_description/base_footprint"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/base_footprint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
